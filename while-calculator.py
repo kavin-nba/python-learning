2581eb3e226a976cd66161ea1b00fb8449ebea80
kavin-nba <kavin.nba@gmail.com>
2021-10-13 16:43:48 +0530

    Refactored while calculator

--- while-calculator.py
+++ while-calculator.py
@@ -1,32 +1,35 @@
 print("the options are + - * / // ** % == != > < ")
 while True:
-    a = int(input('first num: '))
-    op = input('operator: ')
-    c = int(input('second num: '))
-    if op == '+':
-        print(a + c)
-    elif op == '-':
-        print(a - c)
-    elif op == '*':
-        print(a * c)
-    elif op == '/':
-        print(a / c)
-    elif op == '//':
-        print(a // c)
-    elif op == '**':
-        print(a ** c)
-    elif op == '%':
-        print(a % c)
-    elif op == '==':
-        print("num1 is equal to num2")
-    elif op == '!=':
-        print("num1 is not equal to num2")
-    elif op == '>':
-        print("num1 is greater than num2")
-    elif op == '<':
-        print("num1 is is lesser than num2")
-    elif op == 'exit':
+    operator = input('operator: ')
+    if operator == 'exit':
         print("program exit")
         break
+    number1 = int(input('first num: '))
+    number2 = int(input('second num: '))
+    if operator == '+':
+        print(number1 + number2)
+    elif operator == '-':
+        print(number1 - number2)
+    elif operator == '*':
+        print(number1 * number2)
+    elif operator == '/':
+        print(number1 / number2)
+    elif operator == '//':
+        print(number1 // number2)
+    elif operator == '**':
+        print(number1 ** number2)
+    elif operator == '%':
+        print(number1 % number2)
+    elif operator == '==':
+        print("num1 is equal to num2")
+    elif operator == '!=':
+        print("num1 is not equal to num2")
+    elif operator == '>':
+        print("num1 is greater than num2")
+    elif operator == '<':
+        print("num1 is is lesser than num2")
+    # elif operator == 'exit':
+    #     print("program exit")
+    #     break
     else:
         print("wrong operator")
